3428e8b14bf87244d461d1f28f5f1c7ebec53fb1
Jianhui Wang <wjh@rock-chips.com>
2019-11-04 16:53:13 +0800

    rk3368 is use vendor/rockchip/common memtrack.rk3368.so,do not compile
    Change-Id: I35961b07e92eb26002f31491e3caa76a6855f33a
    Signed-off-by: Jianhui Wang <wjh@rock-chips.com>

--- Android.mk
+++ Android.mk
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+ifneq ($(strip $(TARGET_BOARD_PLATFORM)), rk3368)
 LOCAL_PATH := $(call my-dir)
 
 # HAL module implemenation stored in
@@ -35,3 +36,4 @@ LOCAL_SHARED_LIBRARIES :=  liblog
 LOCAL_MODULE := memtrack.$(TARGET_BOARD_PLATFORM)
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
+endif
